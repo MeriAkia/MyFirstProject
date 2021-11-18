ffc09fb2e5de9b9b938ac8651a37dca1d8e83f4e
Meri <meri.akiashvili@gmail.com>
2021-11-18 21:37:29 +0400

    Initial Commit

---
+++ MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Meri Akiashvili on 18.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
